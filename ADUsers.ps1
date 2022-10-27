6f01673d275750713c6d6f23d1a5cbf7789047f7
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 14:51:58 -0400

    Update ADUsers.ps1

--- ADUsers.ps1
+++ ADUsers.ps1
@@ -6,16 +6,16 @@
 
 
 Import-Module ActiveDirectory
-# Set the new password
+#Set the new password
 $newPassword = ConvertTo-SecureString -AsPlainText "MyP@ssw0rd" -Force
-# Import users from CSV
+#Import users from CSV
 Import-Csv ".\ADUsers.csv" | ForEach-Object {
 $samAccountName = $_."samAccountName"
  
-#Un-comment the below line if your CSV file includes password for all users
+#Comment the below line if your CSV file DO NOT include password for all users
 $newPassword = ConvertTo-SecureString -AsPlainText $_."Password"  -Force
  
-# Reset user password.
+#Reset user password.
 Set-ADAccountPassword -Identity $samAccountName -NewPassword $newPassword -Reset
 
 Write-Host " AD Password has been reset for: "$samAccountName
