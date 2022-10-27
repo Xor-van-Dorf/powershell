aa6d2dca0fecd78192c1e4da5648b8295cf2069d
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 11:21:35 -0400

    Add files via upload

---
+++ Old files.ps1
@@ -0,0 +1,8 @@
+﻿$Folder = "C:\Temp"
+
+#Delete files older than 2 months
+Get-ChildItem $Folder -Recurse -Force -ea 0 |
+? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-60)} |
+ForEach-Object {
+   $_ | del -Force
+}
