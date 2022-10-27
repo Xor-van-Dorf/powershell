e99cb119f9c6211b7d705f8be1c54e6b5c70363e
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 09:46:44 -0400

    Add files via upload

---
+++ ADUsers.ps1
@@ -0,0 +1,21 @@
+﻿#Used to set bulk AD passwords
+#CSV file must be:
+#samAccountName|Password
+
+
+
+Import-Module ActiveDirectory
+# Set the new password
+$newPassword = ConvertTo-SecureString -AsPlainText "MyP@ssw0rd" -Force
+# Import users from CSV
+Import-Csv ".\ADUsers.csv" | ForEach-Object {
+$samAccountName = $_."samAccountName"
+ 
+#Un-comment the below line if your CSV file includes password for all users
+$newPassword = ConvertTo-SecureString -AsPlainText $_."Password"  -Force
+ 
+# Reset user password.
+Set-ADAccountPassword -Identity $samAccountName -NewPassword $newPassword -Reset
+
+Write-Host " AD Password has been reset for: "$samAccountName
+}
