527b4e2b801c6d7f417a43d8dc8cb5776074584e
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 11:38:49 -0400

    Delete Modern Auth.ps1

--- Modern Auth.ps1
+++
@@ -1,5 +0,0 @@
-#For Office 2013/2016, it's forcing modern auth
-
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Exchange\" -Name AlwaysUseMSOAuthForAutoDiscover -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity\" -Name EnableADAL -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity\" -Name Version -Value 1 -PropertyType DWORD -Force
