b1369861f38120be202c80631d4865789d6f6d8b
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-11-10 15:34:40 -0500

    Update admin.ps1

--- new_computer/admin.ps1
+++ new_computer/admin.ps1
@@ -29,7 +29,6 @@ net user support $pw
 net user support /expires:never
 
 #####################INSTALL RMM#####################
-
 if ($rmm -eq "y")
 {
     .\rmm.exe
@@ -43,85 +42,49 @@ else
 $name = Read-Host "Veuillez entrer le nom de l'ordinateur"
 Rename-Computer -NewName $name
 
-#####################7-ZIP#####################
-
-Write-Host "Installing 7-Zip..."
-msiexec /i 7z2201-x64.msi /q /norestart
-Start-Sleep -s 10
-
-#####################OPENVPN#####################
-
-Write-Host "Installing OpenVPN..."
-msiexec /i OpenVPN-2.5.7-I602-amd64.msi /qn /norestart
-Start-Sleep -s 10
-
 #####################Disable News and interest#####################
-
 Write-Host "Disabling news and interest..."
 New-Item -Path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows -Name "Windows Feeds" -Force 
 New-ItemProperty -Path "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" -Name EnableFeeds -Value 0 -PropertyType DWORD -Force
 
 #####################DISABLE IPv6#####################
-
 Write-Host "Disabling IPv6..."
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
 
-#####################INSTALL CHROME#####################
-
-Write-Host "Installing Google Chrome..."
-msiexec /i googlechromestandaloneenterprise64.msi /q /norestart
-Start-Sleep -s 30
 #####################DISABLE HIBERFILE.SYS#####################
+Write-Host "Disabling hibernation..."
 powercfg.exe -h off
 
 #####################NO SLEEP ON AC#####################
+Write-Host "Disabling sleep on AC..."
 Powercfg /Change standby-timeout-ac 0
+powercfg /setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+powercfg /setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+
+#####################7-ZIP#####################
+Write-Host "Installing 7-Zip..."
+ECHO Y | winget install 7zip.7zip
+
+#####################INSTALL CHROME#####################
+Write-Host "Installing Google Chrome..."
+winget install Google.Chrome
+
+#####################OPENVPN#####################
+Write-Host "Installing OpenVPN..."
+winget install OpenVPNTechnologies.OpenVPN
+
+#####################ADOBE READER#####################
+Write-Host "Installing Adobe Acrobat Reader..."
+ECHO Y | winget install Adobe.Acrobat.Reader.64-bit
 
 #####################INSTALL OFFICE#####################
-
 Write-Host "Installing Office..."
 $OfficeInstaller = '.\setup.exe'
 $OfficeConfigure = "/configure setup.xml"
 Start-Process -FilePath $OfficeInstaller -ArgumentList $OfficeConfigure -Wait
 
-#####################START ADOBE READER INSTALLER#####################
-
-Write-Host "Installing Adobe Acrobat Reader..."
-Write-Host 'Downloading Files...'
-wget "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2200220191/AcroRdrDC2200220191_fr_FR.exe" -OutFile "AcroRdrDC2200220191_fr_FR.exe"
-
-Write-Host 'Extracting Files...'
-Start-Process -FilePath .\AcroRdrDC2200220191_fr_FR.exe -ArgumentList '-sfx_o"C:\AdobeReaderExtracted" -sfx_ne.\AcroRdrDC2200220191_fr_FR.exe -sfx_o"C:\AdobeReaderExtracted" -sfx_ne' -Wait
-
-Write-Host 'Creating Admin install...'
-Start-Process -FilePath msiexec -ArgumentList '/a c:\AdobeReaderExtracted\AcroRead.msi TARGETDIR="c:\AcrobatReaderDC_AIP" /quiet' -Wait
-
-Write-Host 'Patching Admin install...'
-Start-Process -FilePath msiexec -ArgumentList '/a C:\AcrobatReaderDC_AIP\AcroRead.msi /p C:\AdobeReaderExtracted\AcroRdrDCUpd2200220191.msp /quiet' -Wait
-
-Write-Host 'Creating setup.ini'
-New-Item C:\AcrobatReaderDC_AIP\setup.ini
-Set-Content C:\AcrobatReaderDC_AIP\setup.ini @"
-[Startup]
-RequireMSI=3.0
-CmdLine=/sall /rs
-
-[Product]
-msi=AcroRead.msi
-"@
-
-Write-Host 'Installing...'
-
-
-Write-Host 'Rename folder, cleanup and installing'
-Rename-Item C:\AcrobatReaderDC_AIP C:\AcrobatReaderDC_2022.002.20191
-Move-Item C:\AcrobatReaderDC_2022.002.20191 .\AcrobatReaderDC_2022.002.20191
-msiexec /i "c:\script\AcrobatReaderDC_2022.002.20191\AcroRead.msi" /q /norestart
-Remove-Item C:\AdobeReaderExtracted -Force -Recurse
-Remove-Item .\AcroRdrDC2200220191_fr_FR.exe -Force -Recurse
-
 #####################WINDOWS UPDATE#####################
-
 if ($WindowsUpdate -eq "y")
 {
 	ECHO O | powershell Install-Module PowerShellGet -Force
@@ -133,3 +96,17 @@ else
 {
    write-host "Vous avez choisi de ne pas installer Windows Update"
 }
+
+#Disable services
+Get-Service IntelAudioService | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service Intel(R) Capability Licensing Service TCP IP Interface | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service cplspcon | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service jhi_service | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service esifsvc | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service igccservice | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service igfxCUIService2.0.0.0 | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service WMIRegistrationService | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service Intel(R) TPM Provisioning Service | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service WavesSysSvc | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service WavesAudioService | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service RtkAudioUniversalService | Stop-Service -PassThru | Set-Service -StartupType disabled
